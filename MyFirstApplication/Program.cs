0f085b62739d0d03b0065726063be07941295908
Philip J. Smith <psmith2@wsutech.edu>
2022-03-23 17:03:14 -0500

    CAC-103 Exercise 1 Introduction to CSharp

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -20,3 +20,20 @@ void HouseExample()
     myHouse.DoorOpenClose();
 }
 
+/*
+ This local function is for my first exercise lab.
+ */
+
+
+     void MyLocalFunction()
+{ 
+    Console.WriteLine("This is my first sentence");
+    Console.WriteLine("I am new to CSharp");
+    Console.WriteLine("This CSharp course is cool");
+    Console.WriteLine("I am learning stuff every day.");
+}
+    MyLocalFunction();
+
+
+
+ 
