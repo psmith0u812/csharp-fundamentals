796402b4e737240b61aef3b88a412d38320ee431
Philip J. Smith <psmith2@wsutech.edu>
2022-03-23 13:43:21 -0500

    CAC-103 C# Overview

---
+++ MyFirstApplication/House.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MyFirstApplication;
+
+public class House
+{
+    string foundation;
+    string roofType;
+    string doorColor;
+    int windowSize;
+
+    public void DoorOpenClose()
+    {
+        Console.WriteLine("My door is open");
+    }
+}
+
--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MyFirstApplication;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// The below statement calls a local function
+// single line comment
+HouseExample();
+
+/*
+ Multi-line comment
+The below statement is a local function, local functions
+are inside of members and are private by default. This means
+they are specific to that member and can only be called
+inside that member.
+ */
+void HouseExample()
+{ 
+    House myHouse = new House();
+    myHouse.DoorOpenClose();
+}
+
