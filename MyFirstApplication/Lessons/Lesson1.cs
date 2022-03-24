d6c3d4ed79c6762ea8b338fc02e8c5d13c4dcc52
Philip J. Smith <psmith2@wsutech.edu>
2022-03-23 23:31:02 -0500

    CAC-103 Lesson1 Value Types

---
+++ MyFirstApplication/Lessons/Lesson1.cs
@@ -0,0 +1,148 @@
+﻿
+namespace MyFirstApplication;
+
+/*
+This class goes over Value Types.
+ */
+internal class Lesson1
+{
+    // Integral Signed
+
+    // sbyte range -128 to 127
+    sbyte mySByte = 2;
+
+    public void SByteMinMax()
+    {
+        Console.WriteLine(SByte.MinValue);
+        Console.WriteLine(SByte.MaxValue);
+    }
+
+    // short range -32,768 to 32,767
+    short myShort = 6;
+
+    public void ShortMinMax()
+    {
+        Console.WriteLine(Int16.MinValue);
+        Console.WriteLine(Int16.MaxValue);  
+    }
+
+    // int range -2,147,483,648 to 2,147,483,647
+    int myint = 25685;
+
+    public void IntMinMax()
+    {
+        Console.WriteLine(Int32.MinValue);  
+        Console.WriteLine(Int32.MaxValue);
+    }
+
+    // long range -9,223,372,036,854,775,808 to
+    // 9,223,372,036,854,775,807
+    long mylong = 123456789L;
+
+    public void LongMinMax()
+    {
+        Console.WriteLine(Int64.MinValue);  
+        Console.WriteLine(Int64.MaxValue);
+    }
+
+    // Integral Unsigned
+
+    // byte range 0 - 255 
+    byte myByte = 4;
+
+    public void ByteMinMax()
+    {
+        Console.WriteLine(Byte.MinValue);
+        Console.WriteLine(Byte.MaxValue);   
+    }
+
+    // ushort range 0 - 65,535
+    ushort myUShort = 42;
+
+    public void UShortMinMax()
+    {
+        Console.WriteLine(UInt16.MinValue);
+        Console.WriteLine(UInt16.MaxValue);
+    }
+
+    // uint range 0 -4,294,967,295
+    uint myUInt = 4532U;
+
+    public void UIntMinMax()
+    {
+        Console.WriteLine(UInt32.MinValue);
+        Console.WriteLine(UInt32.MaxValue);
+    }
+
+    // ulong range 0 - 18,446,744,073,709,551,615
+    ulong myULong = 948372UL
+
+        public void ULongMinMax()
+    {
+        Console.WriteLine(UInt64.MinValue); 
+        Console.WriteLine(UInt64.MaxValue);
+    }
+
+    // Floating Point / Real Numbers
+
+    // float range 3.4E +/- 38
+    float myFloat = -123.6589F;
+
+    public void FloatMinMax()
+    {
+        Console.WriteLine(Single.MinValue);
+        Console.WriteLine(Single.MaxValue);
+    }
+
+    // Double range 1.7E +/- 308
+    double myDouble = 12.34D;
+
+    public void DoubleMinMax()
+    {
+        Console.WriteLine(Double.MinValue);
+        Console.WriteLine(Double.MaxValue);
+    }
+
+    // decimal range -7.9228E+24 to 7.9228E+24
+    decimal myDecimal = 13.23M;
+
+    public void DecimalMinMax()
+    {
+        Console.WriteLine(Decimal.MinValue);
+        Console.WriteLine(Decimal.MaxValue);    
+    }
+
+    // Integral Literals
+    public void IntegralExample()
+    {
+        // value of 26
+        int value1 = 26; // decimal
+        int value2 = 0x1a; //hexadecimal
+        int value3 = 0b11010; // binary
+        Console.WriteLine(value1);
+        Console.WriteLine(value2);
+        Console.WriteLine(value3);
+    }
+
+    // Scientific Notation
+    double alpha = 6.26E-22;
+
+    // Underscores
+    long beta = 1_834_432L;
+
+    // Explicit casting is where fractions are truncated and not rounded
+    // You have to provide permission to cast from a higher type 
+    // to a lower type.
+    public void ConvertFloatToInt()
+    {
+        int converted = (int)myFloat;
+        Console.WriteLine(converted);
+    }
+
+    // Implicit cast requires no direct permission
+    public void LongFromInt()
+    {
+        long someValue = myint;
+        Console.WriteLine(someValue);
+    }
+}
--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,11 +1,11 @@
-﻿using MyFirstApplication;
+﻿using Lesson1;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 // The below statement calls a local function
 // single line comment
-HouseExample();
+
 
 /*
  Multi-line comment
@@ -14,11 +14,28 @@ are inside of members and are private by default. This means
 they are specific to that member and can only be called
 inside that member.
  */
+
+// Lesson Examples
+// HouseExample();
+Lesson1Example();
+
 void HouseExample()
 { 
     House myHouse = new House();
     myHouse.DoorOpenClose();
 }
+
+// Lesson 1 Value Types
+void Lesson1Example()
+{
+    Lesson1 myLesson = new Lesson1();
+    myLesson.IntMinMax();
+    myLesson.IntegralExample();
+    myLesson.ConvertFloatToInt();
+    myLesson.LongFromInt();
+}
+
+
 
 /*
  This local function is for my first exercise lab.
