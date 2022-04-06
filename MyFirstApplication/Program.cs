544f90d94a29a9328ccceb8097caf8f851482237
Philip J. Smith <psmith2@wsutech.edu>
2022-04-05 22:32:09 -0500

    CAC-103 Lesson6 Constructors, Methods and more

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -26,7 +26,55 @@ inside that member.
 // Lesson4Example();
 // Exercise4();
 // Lesson5Example();
-Exercise5();
+// Exercise5();
+Lesson6Examples();
+
+
+void Lesson6Examples()
+{
+    // Default Constructor
+    Lesson6 myLesson6 = new Lesson6();
+    Lesson6 myLessonHats = new Lesson6("Cowboy", 7);
+    Lesson6.TryOn theHat = myLessonHats.TryOnHat;
+    theHat($"I tried on a {myLessonHats.HatType} hat that was size {myLessonHats.HatSize}");
+    Console.WriteLine();
+
+    // Constructor with 1 parameter
+    Lesson6 myOther6 = new Lesson6(22);
+
+    // Constructor with 2 Parameters
+    Lesson6 myHats = new Lesson6("Cowboy", 7);
+    Console.WriteLine(myHats.HatSize); // getting value
+    myHats.HatSize = 6; // setting value
+
+    // Method Parameter Modifiers
+    int able = 33, beta = 22, charlie;
+
+
+    myLesson6.InExample(able);
+    myLesson6.RefExample(ref beta);
+    Console.WriteLine(beta);
+    myLesson6.OutExample(out charlie);
+    Console.WriteLine(charlie);
+
+
+    // Multi-Cast Delegate
+    Lesson6.TryOn theNewHat, hangTheHat, multiHat;
+
+    theNewHat = myLesson6.TryOnHat;
+    hangTheHat = myLesson6.HangUpHat;
+    multiHat = theNewHat + hangTheHat;
+
+    theNewHat($"Trying a {myLessonHats.HatType} hat");
+    hangTheHat($"Hanging up my {myLessonHats.HatType} hat");
+    multiHat($"My {myLessonHats.HatType} hat");
+
+
+
+
+}
+
+
 
 
 void Exercise5()
